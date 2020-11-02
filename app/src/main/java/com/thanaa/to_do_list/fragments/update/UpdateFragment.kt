96f9a14a5670e0a591f19a0c6f2591bf01f92512
ThanaCS <Thana_cs@hotmail.com>
2020-11-02 12:09:51 +0300

    Initial Layouts

--- app/src/main/java/com/thanaa/to_do_list/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/thanaa/to_do_list/fragments/update/UpdateFragment.kt
@@ -1,21 +1,21 @@
-package com.thanaa.to_do_list
+package com.thanaa.to_do_list.fragments.update
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.thanaa.to_do_list.R
 
 
-class ListFragment : Fragment() {
-
+class UpdateFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
 
