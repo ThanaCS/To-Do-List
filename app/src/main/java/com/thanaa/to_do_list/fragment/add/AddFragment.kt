1a73c52696150b9980d6ed80f3bc53ec7ca725f7
ThanaCS <Thana_cs@hotmail.com>
2020-11-03 09:31:07 +0300

    Designing the layout for the list

--- app/src/main/java/com/thanaa/to_do_list/fragment/add/AddFragment.kt
+++ app/src/main/java/com/thanaa/to_do_list/fragment/add/AddFragment.kt
@@ -2,8 +2,10 @@ package com.thanaa.to_do_list.fragment.add
 
 import android.os.Bundle
 import android.text.format.DateFormat
+import android.util.Log
 import android.view.*
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -28,6 +30,8 @@ class AddFragment : Fragment(), DatePickerFragment.Callbacks {
     private val mSharedViewModel: SharedViewModel by viewModels()
     private lateinit var todo: TodoData
     private lateinit var dateButton: Button
+    private lateinit var completedCheckBox: CheckBox
+    private var isComplete = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val current = LocalDateTime.now()
@@ -46,11 +50,16 @@ class AddFragment : Fragment(), DatePickerFragment.Callbacks {
         val view = inflater.inflate(R.layout.fragment_add, container, false)
         setHasOptionsMenu(true)
         dateButton = view.findViewById(R.id.dateButton) as Button
+        completedCheckBox = view.findViewById(R.id.isCompleted) as CheckBox
         dateButton.setOnClickListener {
             DatePickerFragment.newInstance(todo.date).apply {
                 setTargetFragment(this@AddFragment, REQUEST_DATE)
                 show(this@AddFragment.requireFragmentManager(), DIALOG_DATE)
             }
+        }
+
+        completedCheckBox.setOnClickListener {
+            isComplete = true
         }
 
         return view
@@ -72,12 +81,11 @@ class AddFragment : Fragment(), DatePickerFragment.Callbacks {
         val mTitle = title.text.toString()
         val mDescription = description.text.toString()
         val mDate = DateFormat.format("EEE, MMM, dd", todo.date).toString()
-        val mIsCompleted = isCompleted.text.toString().toBoolean()
-
         val validation = mSharedViewModel.verifyDataFormUser(mTitle, mDescription)
         //not null insert to database
         if (validation) {
-            val newData = TodoData(0, mTitle, mDescription, todo.date, mIsCompleted)
+            Log.d(TAG, "$isComplete")
+            val newData = TodoData(0, mTitle, mDescription, todo.date, isComplete)
             mToDoViewModel.insertData(newData)
             Toast.makeText(requireContext(), "Successfully Added ", Toast.LENGTH_SHORT).show()
             findNavController().navigate((R.id.action_addFragment_to_listFragment))
